814f5dc3e29aee854348d4fd6a1c7e8f4f85802c
Tyler Hosser <tyler.hosser@libertymutual.com>
2024-04-29 14:01:47 -0400

    users GET endpoint working

--- routes/users/users.js
+++ routes/users/users.js
@@ -1,8 +1,20 @@
 import express from "express";
+import {client, connect} from './db.js';
+const dbName = 'usersdb';
+const collectionName = 'users';
+connect(); // Connect to MongoDB
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    res.send("GET called");    
+    try {
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+        const prj = {user:1,email:1, _id:0};
+        const users = await collection.find({}).project(prj).toArray();
+        res.json(users);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+        }    
 });
 
 router.post('/', async (req, res) => {
