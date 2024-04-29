c891d24b8e56dd41261cd47f199f5e014a0f0c6d
Tyler Hosser <tyler.hosser@libertymutual.com>
2024-04-29 14:08:46 -0400

    users/authenticate POST endpoint working

--- routes/users/users.js
+++ routes/users/users.js
@@ -17,8 +17,24 @@ router.get('/', async (req, res) => {
         }    
 });
 
-router.post('/', async (req, res) => {
-    res.send("POST called");  
-});
+router.post('/authenticate', async (req, res) => {
+    try {
+      const db = client.db(dbName);
+      const collection = db.collection(collectionName);
+      const loginCredentials = req.body;
+      const result = await collection.findOne({"user":loginCredentials.user});
+      if( result ){
+        if(result.password === loginCredentials.password){
+          res.status(200).send("user passed authentication!")
+        }else{
+          res.status(401).send("unauthorized: bad password");
+        }
+      }else{
+        res.status(401).send("unauthorized: user not found");
+      } 
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+  });
 
 export default router;
