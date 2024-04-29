eccfee091711181eed43ae5006e3634c84397eb7
Tyler Hosser <tyler.hosser@libertymutual.com>
2024-04-29 14:18:11 -0400

    gueries POST & GET endpoints working

--- routes/queries/queries.js
+++ routes/queries/queries.js
@@ -1,12 +1,30 @@
 import express from "express";
+import fs from 'fs';
+
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    res.send("GET called");    
+router.get('/', (req, res) => {
+    try {
+        const data = fs.readFileSync('queries.json', 'utf8');
+        res.status(200).send(data);
+    } catch (err) {
+        if (res.status)
+            console.error(err);
+        res.status(404).send("queries.json file not found");
+    }
 });
 
-router.post('/', async (req, res) => {
-    res.send("POST called");  
+router.post('/', (req, res) => {
+    const queryArray = req.body;
+    const data = JSON.stringify(queryArray, null, 2);
+    try {
+        fs.writeFileSync('queries.json', data);
+        console.log('query array saved to queries.json');
+        res.status(200).send("query array saved");
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(err);
+    }
 });
 
 export default router;
